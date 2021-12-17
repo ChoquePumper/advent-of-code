e26e5869ed50d6fc1055bd082ea85008da76166a
ChoquePumper <andreschoquelopez@gmail.com>
2021-12-17 15:10:04 -0300

    AoC2021.day17: fixes for part 1; solved part 2.

--- y2021/day17/puzzle2.lua
+++ y2021/day17/puzzle2.lua
@@ -1,4 +1,4 @@
--- Advent of Code 2021, day 17, part 1
+-- Advent of Code 2021, day 17, part 2
 local function step(x,y, vel_x,vel_y)
 	local dest_x, dest_y = x+vel_x, y+vel_y
 	-- Returns: x and y of destination point
@@ -29,6 +29,7 @@ local function checkHit(x,y, area)
 end
 
 local function shotProbe(vel_x, vel_y, area)
+	--print("Shot probe", vel_x, vel_y, table.concat({area.x1,area.x2,area.y1,area.y2},","))
 	local ever_hit, flag_stop = false, false
 	local x,y = 0,0
 	local max_y = 0
@@ -36,7 +37,8 @@ local function shotProbe(vel_x, vel_y, area)
 		x,y,vel_x,vel_y = step(x,y,vel_x,vel_y)
 		max_y = math.max(max_y, y)
 		local miss_by_x, miss_by_y
-		ever_hit, miss_by_x, miss_by_y = checkHit(x, y, area) -- Fixed: don't pass velocities to checkHit
+		ever_hit, miss_by_x, miss_by_y = checkHit(x, y, area)
+		--print("*", x,y,ever_hit, miss_by_x, miss_by_y)
 		flag_stop = ever_hit or miss_by_x>0 or miss_by_y<0
 	end
 	return ever_hit, max_y
@@ -47,34 +49,34 @@ function solvePart1(target_x1, target_x2, target_y1, target_y2)
 	assert(target_y1 < target_y2)
 	local area = {x1=target_x1, x2=target_x2, y1=target_y1, y2=target_y2}
 	print("area", area.x1, area.x2, area.y1, area.y2)
-	-- Find possible initial x velocities
+	-- Find the minimum and maximum x velocities
 	local vel_x_t = {}
 	local flag_stop = false
-	local tmp_x, tmp_vel_x = 0, 1
+	local tmp_x, min_vel_x = 0, 1
 	repeat
-		tmp_x = tmp_x + tmp_vel_x
+		tmp_x = tmp_x + min_vel_x
 		local hit, miss_by_x = checkHitX(tmp_x, area)
 		if hit then
-			table.insert(vel_x_t, tmp_vel_x)
+			table.insert(vel_x_t, min_vel_x)
 		end
 		flag_stop = miss_by_x > 0
-		tmp_vel_x = tmp_vel_x + 1
+		tmp_vel_x = min_vel_x + 1
 	until flag_stop
-	assert(#vel_x_t > 0, "No initial x velocities found.")
+	local max_vel_x = area.x2
 
-	-- Find the initial (x and y) velocities that makes the probe go to the highest point possible
-	local maximum_y = 0
-	for _,vel_x in ipairs(vel_x_t) do
-		local tmp_vel_y = math.abs(area.y1)-1
-		for vel_y=tmp_vel_y, tmp_vel_y-math.abs(area.y2-area.y1), -1 do
-			local hit, max_y = shotProbe(vel_x, vel_y, area)
+	-- Count all the initial velocities that make the probe hit the target
+	local found = 0
+	for vel_x=min_vel_x, max_vel_x do
+		for vel_y=area.y1, math.abs(area.y1)-1, 1 do
+			local hit = shotProbe(vel_x, vel_y, area)
 			if hit then
-				maximum_y = math.max(maximum_y, max_y)
+				found = found + 1
 			end
 		end
 	end
 	-- Return the answer
-	return maximum_y
+	assert(found > 0)
+	return found
 end
 
 if _VERSION>="Lua 5.3" and not unpack then unpack = table.unpack end
@@ -90,8 +92,8 @@ local function main(filename)
 	local f = assert(io.open(filename)) -- Open file
 	local input = f:read("*a") -- A list with the values
 	f:close() -- Close file
-	local part1answer = solvePart1(inputToParams(input))
-	print("Part 1: answer", part1answer)
+	local part2answer = solvePart1(inputToParams(input))
+	print("Part 2: answer", part2answer)
 end
 
 function test(test_input, expected_value)
@@ -103,7 +105,7 @@ end
 
 if arg then
 	local test_input = "target area: x=20..30, y=-10..-5"
-	test(test_input, 45) -- Run a test
+	test(test_input, 112) -- Run a test
 	-- Run with the specified input file from argument 1
 	if arg[1] then main(arg[1]) end
 end
